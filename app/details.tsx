26b6b50650802ea90c8280b80e619fb333033dc0
ervenderr <ervenidjad12@gmail.com>
2024-10-03 08:45:35 +0800

    initial commit

---
+++ app/details.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const details = () => {
+  return (
+    <View>
+      <Text>details</Text>
+    </View>
+  )
+}
+
+export default details
+
+const styles = StyleSheet.create({
+  
+})
